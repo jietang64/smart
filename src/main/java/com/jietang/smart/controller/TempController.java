5ee30446c9ab49e44ac38eb51ec1e5a7c24a027c
obu_jcfa008 <obu_jcfa008@iflytek.com>
2021-02-20 18:01:43 +0800

    feat: 集成swagger ui

--- src/main/java/com/jietang/smart/controller/TempController.java
+++ src/main/java/com/jietang/smart/controller/TempController.java
@@ -1,6 +1,8 @@
 package com.jietang.smart.controller;
 
 import com.jietang.smart.service.TempService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class TempController {
     @Autowired
     private TempService service;
+
     @GetMapping("/test")
+    @ApiOperation(value = "测试方法")
     public String test() {
         String result = service.test();
         return "success";
